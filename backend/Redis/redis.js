8d2a154dedaa6927e978f40ec89875f89b78354e
HamzaHadis <hamzahadis@users.noreply.github.com>
2023-04-25 19:22:05 +0530

    err solved

---
+++ backend/Redis/redis.js
@@ -0,0 +1,16 @@
+import { createClient } from 'redis';
+
+const client = createClient({
+    password: 'KWiNgp0oUiWwV5RWPj5G4p54ZBePAUDN',
+    socket: {
+        host: 'redis-16056.c305.ap-south-1-1.ec2.cloud.redislabs.com',
+        port: 16056
+    }
+});
+
+client.on('connect', () => {
+    console.log('Redis client connected');
+}
+);
+
+export default client;
