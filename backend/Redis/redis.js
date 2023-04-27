87d90b86010b7d2132381ce416483c68192d7573
HamzaHadis <hamzahadis@users.noreply.github.com>
2023-04-27 23:56:27 +0530

    erro resolved

--- backend/Redis/redis.js
+++ backend/Redis/redis.js
@@ -1,10 +1,10 @@
 import { createClient } from 'redis';
 
 const client = createClient({
-    password: 'KWiNgp0oUiWwV5RWPj5G4p54ZBePAUDN',
+    password: 'q6AZAo0zLZwR3S8c6TKEEcDoBYWRWTaQ',
     socket: {
-        host: 'redis-16056.c305.ap-south-1-1.ec2.cloud.redislabs.com',
-        port: 16056,
+        host: 'redis-10296.c301.ap-south-1-1.ec2.cloud.redislabs.com',
+        port: 10296,
         timeout: 2147483647
     }
 });
