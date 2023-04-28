c50a6571ce6c9a4a0aa79447c51aa76353d97288
Yash-Tulsani <tulsaniyogi@gmail.com>
2023-04-28 08:19:35 +0530

    added docker-compose

--- backend/test/test.js
+++ backend/test/test.js
@@ -1,56 +1,24 @@
-import chai from 'chai';
-const expect = chai.expect;
-
 import app from '../app.js';
-import mongoose from 'mongoose';
-import Review from '../public/models/review.js';
+import request from 'supertest';
+import { assert,expect } from 'chai';
 
-describe('Reviews API Test', () => {
 
-  before(async () => {
-    await mongoose.connect('mongodb://localhost:27017/lep_test', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
+describe('/api/find/search', () => {
+    it('should return a list of employees matching the search criteria', async () => {
+      // send a POST request to the endpoint with search criteria
+      const searchCriteria = {
+        pincode: 121212,
+        skills: 'Barber'
+      };
+      const res = await request(app)
+        .post('/api/find/search')
+        .send(searchCriteria);
+  
+      // check the response status and body
+      assert.strictEqual(res.statusCode, 200);
+      expect(res.body).to.be.an('array').that.has.lengthOf(1);
+      expect(res.body[0].fname).to.equal('yash');
+      expect(res.body[0].skills).to.equal('Barber');
+      expect(res.body[0].pincode).to.include(searchCriteria.pincode);
     });
   });
-
-  after(async () => {
-    await mongoose.connection.close();
-  });
-
-  describe('GET /api/other/allReview', () => {
-    it('should return an array of reviews', (done) => {
-      Review.create(
-        {
-          name: 'John Doe',
-          profession: 'Engineer',
-          gender: 'Male',
-          comment: 'This is a test review.',
-          stars: 5,
-        },
-        (err, review) => {
-          if (err) {
-            console.error(err);
-            done();
-          } else {
-            chai
-              .request(app)
-              .get('/api/other/allReview')
-              .end((err, res) => {
-                expect(res).to.have.status(200);
-                expect(res.body).to.be.an('array');
-                expect(res.body).to.have.lengthOf(1);
-                expect(res.body[0].name).to.equal('John Doe');
-                expect(res.body[0].profession).to.equal('Engineer');
-                expect(res.body[0].gender).to.equal('Male');
-                expect(res.body[0].comment).to.equal('This is a test review.');
-                expect(res.body[0].stars).to.equal(5);
-                done();
-              });
-          }
-        }
-      );
-    });
-  });
-
-});
